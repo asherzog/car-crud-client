418eb51080169623a38dcc2393cd71776c81b5ff
Andy Herzog <andyherzog@Andys-MacBook-Pro.local>
2016-12-16 14:09:10 -0700

    created initial index page

---
+++ scripts/user.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  console.log('hi');
+  // const id = parseQueryString(window.locoation.search);
+  // console.log(id);
+
+
+});
+
+
+// function parseQueryString(queryString) {
+//   let answer = {};
+//   let splitStr = queryString.split('&');
+//   splitStr.forEach((str) => {
+//     let pair = str.replace('?', '');
+//     pair = pair.split('=');
+//     answer[pair[0]] = pair[1];
+//   });
+//   return answer;
+// }
