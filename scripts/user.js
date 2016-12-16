80ea2584979ca499f5584373da0c2c80954e8685
dashlw <daisha.walton@du.edu>
2016-12-16 15:30:48 -0700

    added images deployed to firebase link heroku

--- scripts/user.js
+++ scripts/user.js
@@ -1,34 +1,47 @@
-const API_URL = ''
+const API_URL = getHostURL();
 
 $(document).ready(function() {
-  const query = parseQueryString(window.location.search);
+    const query = parseQueryString(window.location.search);
 
-  $.get(`http://localhost:3000/user/${query.id}`, function(data) {
-    console.log(data);
-    $('.user').append(`<h1>${data.email}</h1>`);
-  });
-
-  $.get(`http://localhost:3000/user/${query.id}/car`, function(data) {
-    data.forEach(function(car) {
-      $('.cars').append(`
-        <h1>${car.make}</h1>
-        <h2>${car.model}</h2>
-        <p>${car.year}</p>
-        `);
+    $.get(`${API_URL}user/${query.id}`, function(data) {
+        console.log(data);
+        $('.user').append(`<h1>${data.name}</h1>`);
     });
-  });
 
-
+    $.get(`${API_URL}user/${query.id}/car`, function(data) {
+        data.forEach(function(car) {
+            $('.cars').append(`
+              <div class="media">
+                <div class="media-left">
+                  <a href="#">
+                    <img class="media-object" src="${car.image}" alt="...">
+                  </a>
+                </div>
+                <div class="media-body">
+                  <h4 class="media-heading">${car.make}, ${car.model}</h4>
+                </div>
+              </div>
+        `);
+        });
+    });
 });
 
 
 function parseQueryString(queryString) {
-  let answer = {};
-  let splitStr = queryString.split('&');
-  splitStr.forEach((str) => {
-    let pair = str.replace('?', '');
-    pair = pair.split('=');
-    answer[pair[0]] = pair[1];
-  });
-  return answer;
+    let answer = {};
+    let splitStr = queryString.split('&');
+    splitStr.forEach((str) => {
+        let pair = str.replace('?', '');
+        pair = pair.split('=');
+        answer[pair[0]] = pair[1];
+    });
+    return answer;
+}
+
+function getHostURL() {
+    if (window.location.host.indexOf('localhost') != -1) {
+        return 'http://localhost:3000';
+    } else {
+        return 'https://carcrud.herokuapp.com/';
+    }
 }
